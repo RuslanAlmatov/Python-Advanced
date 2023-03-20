26431925e522ea779283250b68a6c3f316b1e68a
RuslanAlmatov <115878956+RuslanAlmatov@users.noreply.github.com>
2023-03-20 17:14:46 +0500

    Add files via upload

--- mod4/mod45/current_process.py
+++ mod4/mod45/current_process.py
@@ -1,4 +1,6 @@
 import shlex
+import string
+import subprocess
 
 from flask import Flask, request
 
@@ -9,7 +11,15 @@ app = Flask(__name__)
 def current_process():
     args: list[str] = request.args.getlist('arg')
     cleaned_args = [shlex.quote(i) for i in args]
-    return f"ps <pre>{''.join(cleaned_args)}</pre>"
+    command_str = f"ps {' '.join(cleaned_args)}"
+    command = shlex.split(command_str)
+    result = subprocess.run(command, capture_output=True)
+
+    if result.returncode != 0:
+        return "Something went wrong", 500
+
+    output = result.stdout.decode()
+    return string.Template(f"ps <pre>${output}</pre>").substitute(output=output)
 
 
 if __name__ == "__main__":
